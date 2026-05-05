e8c68592bf10bb743e70865c21fdb6bfa2589d26
Mateen Waqar <connect.mateenwaqar@gmail.com>
2026-05-05 23:46:37 +0500

    fix template rendering

--- api/server.py
+++ api/server.py
@@ -1,8 +1,7 @@
-from flask import Flask
-from flask import Flask, send_from_directory
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="../templates")
 
 @app.route("/")
 def home():
-    return send_from_directory(".", "index.html")
+    return render_template("index.html")
